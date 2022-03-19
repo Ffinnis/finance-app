c4ac3d91c0f050a609ce301d40160ddf58170179
Ffinnis <potapovroman03@gmail.com>
2022-03-19 22:33:02 +0900

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { ViteAliases } from 'vite-aliases'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/finance-app/',
   plugins: [vue(), ViteAliases()],
   resolve: {
     alias: {
