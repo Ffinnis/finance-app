f9165d90de9c64ea93c4626b1e096707c8ced087
Ffinnis <potapovroman03@gmail.com>
2022-03-22 23:58:15 +0900

    deploy fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,27 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../pages/Home.vue'
-import Income from '../pages/Income.vue'
-import Costs from '../pages/Costs.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../pages/Home.vue";
+import Income from "../pages/Income.vue";
+import Costs from "../pages/Costs.vue";
 
 const routes = [
-    {
-        path: '/',
-        name: 'Home',
-        component: Home
-    },
-    {
-        path: '/income',
-        name: 'Income',
-        component: Income
-    },
-    {
-        path: '/costs',
-        name: Costs,
-        component: Costs
-    }
-]
+  {
+    path: "/finance-app",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/income",
+    name: "Income",
+    component: Income,
+  },
+  {
+    path: "/costs",
+    name: Costs,
+    component: Costs,
+  },
+];
 const router = createRouter({
-    routes,
-    history: createWebHistory()
-})
-export default router
+  routes,
+  history: createWebHistory(),
+});
+export default router;
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { ViteAliases } from 'vite-aliases'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { ViteAliases } from "vite-aliases";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: "/finance-app",
   plugins: [vue(), ViteAliases()],
   resolve: {
     alias: {
-      vue: 'vue/dist/vue.esm-bundler.js',
+      vue: "vue/dist/vue.esm-bundler.js",
     },
   },
-})
+});
